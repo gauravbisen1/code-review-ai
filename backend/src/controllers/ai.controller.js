ae44d0473e4ef4f9e85ab15a4bcd1b7d652a2192
gauravbisen1 <gouravbisen123@gmail.com>
2025-07-21 22:31:01 +0530

    backend done!

--- backend/src/controllers/ai.controller.js
+++ backend/src/controllers/ai.controller.js
@@ -1,12 +1,12 @@
 const aiService = require("../services/ai.service");
 
-module.exports.getResponse = async (req,res)=>{
-    const prompt = req.body.prompt;
+module.exports.getReview = async (req,res)=>{
+    const code = req.body.code;
 
-    if (!prompt) {
+    if (!code) {
         return res.status(400).send("prompt is required!");
     }
 
-    const response = await aiService(prompt);
+    const response = await aiService(code);
     res.send(response);
 }
